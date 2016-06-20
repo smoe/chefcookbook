b7459048d86bb0bfa26fa4940ba44afc9d068bf1
Steffen Moeller <moeller@debian.org>
2016-06-20 16:10:29 +0200

    Initial clumsy test

---
+++ boinc/metadata.rb
@@ -0,0 +1,7 @@
+name             'boinc'
+maintainer       'University of Rostock'
+maintainer_email 'moeller@debian.org'
+license          'All rights reserved'
+description      'Installs/Configures BOINC'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.0.1'
---
+++ boinc/recipes/default.rb
@@ -0,0 +1,26 @@
+#
+# Cookbook Name:: boinc
+# Recipe:: default
+#
+# Copyright 2016, Steffen Moeller
+#
+# All rights reserved - Do Not Redistribute
+#
+
+apt_update 'Update the apt cache daily' do
+  frequency 86_400
+  action :periodic
+end
+
+package 'boinc-client' do
+  action :install
+end
+
+service 'boinc-client' do
+  supports :status => true
+  action [ :enable, :start ]
+end
+
+execute 'register with Einstein@Home' do
+  command 'boinccmd --project_attach https://einstein.phys.uwm.edu 11376_ed6638d7db21cc162e96f75c4e54dd7b'
+end
